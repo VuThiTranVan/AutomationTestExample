6f87678e39f8461fb900a6e4d8e07b9a2c05600a
vuthitranvan <vu.thi.tran.van@framgia.com>
2018-07-27 19:04:18 +0700

    Sesion 11: Handing Date Time Picker

---
+++ src/chapter11/Practice_Kendo_UI.java
@@ -0,0 +1,12 @@
+package chapter11;
+
+public class Practice_Kendo_UI {
+
+	public static void main(String[] args) throws InterruptedException {
+		KendoDateTimePicker kendo = new KendoDateTimePicker();
+		kendo.startTest();
+		kendo.SelectDateFromKendoCalendar();
+		kendo.endTest();
+	}
+
+}
